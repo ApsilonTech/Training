63a3b33f8c7995c64d1264dc94da33f377716db5
kishoreck75 <104119412+kishoreck75@users.noreply.github.com>
2022-09-19 11:44:06 +0530

    Add files via upload

---
+++ Kishore/Activity-4/Question9.py
@@ -0,0 +1,19 @@
+#Activity Code: KL/EP-19/A-004    
+#Platform: Python 3.10,windows 10 
+#Author: Mr.Kishore Kumar C       
+#Role: Software Engineer, Apsilon.
+#Date: 19/09/2022                 
+
+# Question number-9:
+
+
+Fname="p1.log"
+def fx():
+    print(Fname)
+Fname="p2.log"
+print(Fname)
+fx()
+print(Fname)
+
+
+
